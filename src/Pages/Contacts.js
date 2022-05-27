96cb9e0bc7da0f409dfc7a8e243f56d1299941fe
Mihail <misha.vydrin@gmail.com>
2022-05-27 15:45:02 +0500

    Добавил форму обратнойсвязи

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,11 +1,33 @@
+
 import React, { Component } from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
 
 export default class Contacts extends Component {
   render() {
     return (
-      <div className='m-5'>
-        Contacts
-      </div>
+      <Container style={{ width: '500px' }}>
+        <h1 className='text-center'>Contact us</h1>
+        <Form>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type='email' placeholder='Enter email' />
+            <Form.Text>
+              We`ll never sgare your email with anyone else
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId='formBasicPassword'>
+            <Form.Label>Example textrea</Form.Label>
+            <Form.Control as="textarea" rows='3' />
+          </Form.Group>
+
+          <Form.Group controlId='formBasicCheckBox'>
+            <Form.Check type='checkbox' label='Check me out' />
+          </Form.Group>
+
+          <Button variant='primary' type='submit'>submit</Button>
+        </Form>
+      </Container>
     )
   }
 }
